028282e7e55bab86e75280f9bbee0cb65e08a7f0
Péter Szilágyi <peterke@gmail.com>
2018-11-09 05:22:02 +0000

    Updated libtor dependencies

--- openssl_config/buildinf.x64.h
+++ openssl_config/buildinf.x64.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-x86_64"
-#define DATE "built on: Fri Nov  9 05:02:09 UTC 2018"
+#define DATE "built on: Fri Nov  9 05:21:35 UTC 2018"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
